ef6959736e58fa0992f88b11a0b9d505157d0717
Niloy <muntasirniloy2002@gmail.com>
2025-10-19 02:20:47 +0600

    final

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://inspiring-creponne-9cb190.netlify.app", 
+    origin: "https://moonlit-gingersnap-992023.netlify.app", 
     methods: ["GET,POST,PUT,DELETE"],
     credentials: true, 
   })
