563dda77d9c59378f901e7e9da7fef966cdd9db4
Niloy <muntasirniloy2002@gmail.com>
2025-10-19 02:19:11 +0600

    otp removed

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const { registration, verifyOTP, login, verifyUser, logout, getAllUsers, deleteAll } = require("../controllers/auth");
+const { registration,  login, verifyUser, logout, getAllUsers, deleteAll } = require("../controllers/auth");
 
 
 const router = express.Router();
 
 router.post('/registration',registration)
-router.post('/verify-otp',verifyOTP)
 router.post('/login',login)
 router.get('/verify-user',verifyUser)
 router.get('/log-out',logout)
