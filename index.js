3d629f9113a6067c677f411bf1ffc74f8b63450a
Niloy <muntasirniloy2002@gmail.com>
2025-10-19 01:13:27 +0600

    fourth commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://inspiring-creponne-9cb190.netlify.app/", 
     methods: "GET,POST,PUT,DELETE",
     credentials: true, 
   })
